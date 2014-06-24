b4af128f027c7e7cdffd4b09341c919915f27e08
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2014-06-24 16:43:02 +0900

    Updated for Matlab R2014a

--- src/libbdbmex.cc
+++ src/libbdbmex.cc
@@ -11,11 +11,27 @@
 
 using mex::MxArray;
 
-/// Hidden MEX API for encoding.
-EXTERN_C mxArray* mxSerialize(const mxArray*);
+// MX_API_VER has unfortunately not changed between R2013b and R2014a,
+// so we use the new MATRIX_DLL_EXPORT_SYM as an ugly hack instead
+//
+#if defined(__cplusplus) && defined(MATRIX_DLL_EXPORT_SYM)
+  #define EXTERN_C extern
+  namespace matrix{
+    namespace detail{
+      namespace noninlined{
+        namespace mx_array_api{
+#endif
 
-/// Hidden MEX API for decoding.
-EXTERN_C mxArray* mxDeserialize(const void*, size_t);
+EXTERN_C mxArray* mxSerialize(mxArray const *);
+EXTERN_C mxArray* mxDeserialize(const void *, size_t);
+
+#if defined(__cplusplus) && defined(MATRIX_DLL_EXPORT_SYM)
+        }
+      }
+    }
+  }
+  using namespace matrix::detail::noninlined::mx_array_api;
+#endif
 
 namespace mex {
 
@@ -295,7 +311,7 @@ bool Database::del(const mxArray* key,
                    uint32_t flags,
                    Transaction* transaction) {
   Record record(key);
-  code_ = database_->del(database_, 
+  code_ = database_->del(database_,
                          (transaction == NULL) ? NULL : transaction->get(),
                          record.key(),
                          flags);
